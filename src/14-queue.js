083bbc65ea16ce0587ee4f0f0cb94b35fa1e5f6d
the-GriS <grishhenjas@gmail.com>
2021-04-13 01:36:44 +0300

    init

--- src/14-queue.js
+++ src/14-queue.js
@@ -12,33 +12,16 @@
  */
 
 class Queue {
-  constructor() {
-    this.head = null;
-    this.tail = null;
-    this.length = 0;
-  }
-
   get size() {
-    return this.length;
+    throw new Error('Not implemented');
   }
 
-  enqueue(element) {
-    const node = new ListNode(element);
-    if (this.head) {
-      this.tail.next = node;
-      this.tail = node;
-    } else {
-      this.head = node;
-      this.tail = node;
-    }
-    this.length++;
+  enqueue(/* element */) {
+    throw new Error('Not implemented');
   }
 
   dequeue() {
-    const del = this.head;
-    this.head = this.head.next;
-    this.length--;
-    return del.value;
+    throw new Error('Not implemented');
   }
 }
 
